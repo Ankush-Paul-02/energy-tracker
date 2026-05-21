7d78e9166655c03031400a0052c87f324277166f
Ankush-Paul-02 <ankushpaulclg2002@gmail.com>
2026-05-22 02:47:57 +0530

    add some basic test

--- deviceservice/src/test/java/com/paul/deviceservice/DeviceserviceApplicationTests.java
+++ deviceservice/src/test/java/com/paul/deviceservice/DeviceserviceApplicationTests.java
@@ -1,14 +1,39 @@
 package com.paul.deviceservice;
 
+import com.paul.deviceservice.data.entities.Device;
+import com.paul.deviceservice.data.enums.DeviceType;
+import com.paul.deviceservice.repository.DeviceRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class DeviceserviceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    private final int numberOfDevices = 200;
+    private final int numberOfUsers = 10;
+
+    @Autowired
+    private DeviceRepository deviceRepository;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void createDevice() {
+        for (int i = 1; i <= numberOfDevices; i++) {
+            Device device = Device.builder()
+                    .name("device" + i)
+                    .type(DeviceType.values()[i % DeviceType.values().length])
+                    .location("location" + i)
+                    .userId((long) (i % numberOfUsers) + 1)
+                    .build();
+            deviceRepository.save(device);
+        }
+        log.info("Devices created successfully");
+    }
 
 }
--- userservice/src/test/java/com/paul/userservice/UserserviceApplicationTests.java
+++ userservice/src/test/java/com/paul/userservice/UserserviceApplicationTests.java
@@ -1,13 +1,38 @@
 package com.paul.userservice;
 
+import com.paul.userservice.data.entities.User;
+import com.paul.userservice.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class UserserviceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    private final int numberOfUsers = 10;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void testUsersToDB() {
+        for (int i = 1; i <= numberOfUsers; i++) {
+            User user = User.builder()
+                    .username("username" + i)
+                    .email("email" + i + "@example.com")
+                    .address("address" + i)
+                    .isAlerting(i % 2 == 0)
+                    .energyAlertingThreshold(1000.0 * i)
+                    .build();
+            userRepository.save(user);
+        }
+        log.info("Users saved to database");
+    }
 
 }
